a6f28b4aedbb865e4c8354f80f31fe4293aa5161
MyLanNguyen999 <mylannguyen192@gmail.com>
2025-03-10 22:08:12 -0400

    Update ar-feature.js

--- js/ar-feature.js
+++ js/ar-feature.js
@@ -56,7 +56,7 @@ const createScene = async function () {
     let sofaMesh = result.meshes[0];
 
     // position the sofa
-    sofaMesh.position = new BABYLON.Vector3(0, 0, 3);
+    sofaMesh.position = new BABYLON.Vector3(0, 0, 0);
 
     // scale the sofa
     sofaMesh.scaling = new BABYLON.Vector3(1.5, 1.5, 1.5);
